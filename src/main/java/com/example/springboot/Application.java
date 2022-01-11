ad1e04c0bc8bd58ea1a9866ba569234affe10a6d
KJY <rntlls1366@gmail.com>
2022-01-11 22:55:40 +0900

    5장까지 완료

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing
 @SpringBootApplication      //스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정, 이 위치부터 설정을 이어나가가기에 최상단 위치
 public class Application {
     public static void main(String[] args) {
--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -1,12 +1,17 @@
 //61p
 package com.example.springboot;
 
+import com.example.springboot.config.auth.SecurityConfig;
 import com.example.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
@@ -15,21 +20,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @RunWith(SpringRunner.class)        //스프링부트 테스트와 JUnit 사이의 연결자
-@WebMvcTest(controllers = HelloController.class)        //웹에 집중할 수 있는 어노테이션
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }        //웹에 집중할 수 있는 어노테이션
+)
 public class HelloControllerTest {
 
     @Autowired      //스프링이 관리하는 빈을 주입받음
     private MockMvc mvc;        //웹 API 테스트, 스프링 MVC 테스트의 시작점
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
-        mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+        //mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
         //MockMvc를 통해 /hello 주소로 HTTP GET 요청 , mvc.perform의 결과와 HTTP Header Status 검증(200,404 등) OK(200)인지 아닌지 , 응답 본문의 내용 검증 hello리턴이 맞는지
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
